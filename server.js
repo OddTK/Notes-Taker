3da5b047de797463816112c3df71fd54463e7d5a
Jesus <jesusbalandra@outlook.com>
2022-05-14 21:48:49 -0600

    create readAndAppend funtion

--- server.js
+++ server.js
@@ -40,9 +40,23 @@ app.post('/api/notes', checkBodyForText, (req, res) => {
 const readFileAsync = util.promisify(fs.readfile);
 
 const writeFileAsync = (destination, content) =>
-    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    fs.writeFileAsync(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
     );
+
+const readAndApend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeFileAsync(file, parsedData);
+        }
+    });
+};
+
+
 
 
 
