1754f9d64c3f9713a71b9953d6a020aff3a05bd8
Jesus <jesusbalandra@outlook.com>
2022-05-14 22:43:28 -0600

    file directory was wrong

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.static("public"));
 
-const uuid = require("./helpers/uuid");
+const uuid = require("./helper/uuid");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
