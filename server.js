d36087f34de27b86e7f9277bc81ada36e8cada4d
Jesus <jesusbalandra@outlook.com>
2022-05-14 21:38:57 -0600

    started readfile and write file in server

--- server.js
+++ server.js
@@ -37,8 +37,12 @@ app.post('/api/notes', checkBodyForText, (req, res) => {
     res.json(newNote);
 });
 
-// const readFileAsync = util.promisify(fs.readfile);
-// const writeFileAsync = util.promisify(fs.write);
+const readFileAsync = util.promisify(fs.readfile);
+
+const writeFileAsync = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
 
 
 
